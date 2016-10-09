388ecfd76ee669db7029dda47ac4e68b13b512f4
Joe Pugh <mishimaltd@gmail.com>
2016-10-09 13:03:55 -0400

    "Fixed closer to initial cut"

--- src/main/java/com/mishima/tracker/Application.java
+++ src/main/java/com/mishima/tracker/Application.java
@@ -1,7 +1,5 @@
 package com.mishima.tracker;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
