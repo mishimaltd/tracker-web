e9c6d6406529b2ad5c55cfa98669c5a0d207cc63
Joe Pugh <mishimaltd@gmail.com>
2016-11-07 20:53:31 -0500

    Added push to heroku

--- src/main/java/com/mishima/tracker/filter/SimpleCORSFilter.java
+++ src/main/java/com/mishima/tracker/filter/SimpleCORSFilter.java
@@ -3,14 +3,16 @@ package com.mishima.tracker.filter;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class SimpleCORSFilter implements Filter {
 
     private static final Logger log = LoggerFactory.getLogger(SimpleCORSFilter.class);
@@ -21,9 +23,8 @@ public class SimpleCORSFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
-        HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
-        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+        response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
         response.setHeader("Access-Control-Max-Age", "3600");
